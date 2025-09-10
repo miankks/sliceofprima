1903a1cd42454900f11e68d6988bc73be31051f5
miankks <bilal.jan085@gmail.com>
2025-09-10 15:26:26 +0200

    contextapi create

---
+++ frontend/src/components/foodDisplay/FoodDisplay.jsx
@@ -0,0 +1,12 @@
+import React, { useContext } from 'react'
+import './FoodDisplay.css'
+import { StoreContext } from '../../context/StoreContext'
+
+const FoodDisplay = () => {
+    const { food_list } = useContext(StoreContext);
+  return (
+    <div>FoodDisplay</div>
+  )
+}
+
+export default FoodDisplay
