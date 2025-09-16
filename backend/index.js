ae8a50fa3c0c3b4724d3edd0982f25501e58a4a6
miankks <bilal.jan085@gmail.com>
2025-09-16 16:33:43 +0200

    connect to mongoDB

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+import path from 'path';
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log("Connected to MongoDB");
+    
+})
+
+const app = express();
+
+app.use(express.json());
+
+app.listen(3000, () => {
+    console.log("Server is running on port 300!");
+    
+})
+
