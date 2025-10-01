861a66f53f6cd7935fc3859c1f951fbeda1afb11
miankks <bilal.jan085@gmail.com>
2025-10-01 15:52:26 +0200

    cart route is enabled

--- api/controllers/cart.controller.js
+++ api/controllers/cart.controller.js
@@ -1,7 +1,13 @@
 import userModel from '../models/user.model.js';
 
 const addToCart = async (req, res) => {
-
+    try {
+        let userData= await userModel.findOne({_id: req.body.userId});
+        let cartData = await userData.cartData;
+        
+    } catch (error) {
+        
+    }
 }
 
 const removeFromCart = async (req, res) => {
@@ -11,3 +17,5 @@ const removeFromCart = async (req, res) => {
 const getCart = async (req, res) => {
 
 }
+
+export {addToCart, removeFromCart, getCart } 
