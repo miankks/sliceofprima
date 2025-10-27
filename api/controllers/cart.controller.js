74c5fac7b97a8fa3f861c4992357acd16d194e99
miankks <bilal.jan085@gmail.com>
2025-10-27 11:28:46 +0100

    fix cart model and add item to cart

--- api/controllers/cart.controller.js
+++ api/controllers/cart.controller.js
@@ -2,10 +2,25 @@ import userModel from '../models/user.model.js';
 
 const addToCart = async (req, res) => {
     try {
-        let userData= await userModel.findOne({_id: req.body.userId});
-        let cartData = await userData.cartData;
+        const { userId, itemId } = req.body;
+        // this middleware will convert the token into userid
+        let userData= await userModel.findOne({_id: userId});
         
+        // extract the cartdata
+        let cartData = await userData.cartData || {};
+        // let cartData = await userData.cartData;
+        // console.log(req.body);
+        
+        !cartData[itemId] ? cartData[itemId] = 1 : cartData[itemId] += 1;
+      
+        
+        //update the user cart with this new cart data
+        await userModel.findByIdAndUpdate(userId, {cartData})
+        res.json({seccess: true, message: "Added to cart"});
+
     } catch (error) {
+        console.log(error);
+        res.json({success: false, message: "Error"})
         
     }
 }
